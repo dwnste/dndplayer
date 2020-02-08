0a15bf116eb363d9f1d84c3a157daffd5c399868
downstate <dwnste@gmail.com>
2020-02-08 20:39:08 +0300

    Fix path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 import 'react-native-gesture-handler'; // TODO: remove when fixed
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import App from './js/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
